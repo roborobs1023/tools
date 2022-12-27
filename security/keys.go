4f4a4b359d219d634011533497cbdc1c0ea302b7
Robert Tucker <roborobs_computers@live.com>
2022-12-27 11:58:28 -0500

    update security

--- security/keys.go
+++ security/keys.go
@@ -5,22 +5,21 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"log"
 	"os"
 )
 
-func GenerateRSAKeys() {
+func GenerateRSAKeys() error {
 	bitSize := 4096
 
 	// Generate RSA key.
 	key, err := rsa.GenerateKey(rand.Reader, bitSize)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	err = key.Validate()
 	if err != nil {
-		panic(err)
+		return err
 	}
 	// Extract public component.
 	pub := key.Public()
@@ -43,37 +42,48 @@ func GenerateRSAKeys() {
 
 	// Write private key to file.
 	if err := os.WriteFile("id_rsa", keyPEM, 0700); err != nil {
-		panic(err)
+		return err
 	}
 	//fmt.Println("Private Key:", string(keyPEM))
 	// Write public key to file.
 	if err := os.WriteFile("id_rsa.pub", pubPEM, 0755); err != nil {
-		panic(err)
+		return err
 	}
 
-	//fmt.Println("Public Key:", string(pubPEM))
+	return nil
 }
 
-
-//CompareKeys takes in a filename of private key and compares it with the 
-func CompareKeys(keyid string) bool {
+// CompareKeys takes in a filename of private key and compares it with the
+func CompareKeys(keyid string) (bool, error) {
 	private, err := os.ReadFile(keyid)
 	if err != nil {
-		log.Fatalln(err)
+		return false, err
 	}
 
 	//fmt.Println("Private Key:", rsaPrivateKey)
 	pub := keyid + ".pub"
 	public, err := os.ReadFile(pub)
 	if err != nil {
-		log.Fatalln(err)
+		return false, err
 	}
-	//rsaPublicKey := string(public)
-	//fmt.Println("Public Key:", rsaPublicKey)
 
-	block, _ := pem.Decode(private)
-	key, _ := x509.ParsePKCS1PrivateKey(block.Bytes)
-	pubBlock, _ := pem.Decode(public)
-	pubKey, _ := x509.ParsePKCS1PublicKey(pubBlock.Bytes)
+	block, err := pem.Decode(private)
+	if err != nil {
+		return false, err
+	}
+
+	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return false, err
+	}
+	pubBlock, err := pem.Decode(public)
+	if err != nil {
+		return false, err
+	}
+	pubKey, err := x509.ParsePKCS1PublicKey(pubBlock.Bytes)
+	if err != nil {
+		return false, err
+	}
+
 	return key.PublicKey.Equal(pubKey)
 }
